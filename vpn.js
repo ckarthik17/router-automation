9b9e0558b1e5e04a849e1835843934389213726d
Karthik Chandraraj <ckarthik17@gmail.com>
2018-04-14 09:03:17 +0800

    [CK] Display IP location after connection

--- vpn.js
+++ vpn.js
@@ -1,45 +1,70 @@
 var webdriver = require('selenium-webdriver');
 var by = webdriver.By;
 var until = webdriver.until;
-let WAIT_MLS = 5000;
+const publicIp = require('public-ip');
+const iplocate = require("node-iplocate");
+
+let WAIT_MLS = 10000;
+let PWD = '';
+let PPTP = 'pptp'
+let DHCP = 'dynamic'
 
 const chromeCapabilities = webdriver.Capabilities.chrome();
-chromeCapabilities.set('chromeOptions', {args: ['--headless']});
+// chromeCapabilities.set('chromeOptions', {args: ['--headless']});
 
-driver = new webdriver.Builder().forBrowser('chrome').withCapabilities(chromeCapabilities).build();
+function setupInet(inetType) {
+    driver = new webdriver.Builder().forBrowser('chrome').withCapabilities(chromeCapabilities).build();
 
-driver.manage().window().maximize();                        
-driver.get('http://192.168.0.1/bsc_wan.php');
+    driver.manage().window().maximize();                        
+    driver.get('http://192.168.0.1/bsc_wan.php');
+    
+    pwdField = driver.findElement(by.id('loginpwd'));
+    pwdField.sendKeys(PWD);
+    
+    loginBtn = driver.findElement(by.id('noGAC'));
+    loginBtn.click();
+    
+    modeId = by.id('wan_ip_mode');
+    
+    driver.wait(until.elementLocated(modeId), WAIT_MLS).then(function(modeElement) {
+        console.log('Login successful');
+        driver.wait(until.elementIsVisible(modeElement), WAIT_MLS).then(function() {
+            modeElement.sendKeys(inetType);
+    
+            topSave = driver.findElement(by.id('topsave'));
+            topSave.click();
+            console.log('Saving');
 
-pwdField = driver.findElement(by.id('loginpwd'));
-pwdField.sendKeys('');
-
-loginBtn = driver.findElement(by.id('noGAC'));
-loginBtn.click();
-
-modeId = by.id('wan_ip_mode');
-
-driver.wait(until.elementLocated(modeId), WAIT_MLS).then(function(el) {
-    modeElement = el;
-    driver.wait(until.elementIsVisible(modeElement), WAIT_MLS).then(function() {
-        modeElement.sendKeys('dynamic');
-
-        topSave = driver.findElement(by.id('topsave'));
-        topSave.click();
-
-        menuId = by.id('menu');
-        driver.wait(until.elementLocated(menuId), WAIT_MLS).then(function(menuEl) {
-            console.log('Menu found');
-            driver.wait(until.elementIsVisible(menuEl), WAIT_MLS).then(function() {
-                console.log('Saved successfully');
-                driver.get('http://192.168.0.1/status.php');
-
-                statusText = driver.findElement(by.id('st_networkstatus'));
-                driver.wait(until.elementTextIs(statusText, 'Connected'), WAIT_MLS).then(function() {
-                    console.log('Connected');
-                    driver.quit();
+            menuId = by.id('menu');
+            driver.wait(until.elementLocated(menuId), WAIT_MLS).then(function(menuEl) {
+                console.log('Loading completed');
+                driver.wait(until.elementIsVisible(menuEl), WAIT_MLS).then(function() {                
+                    driver.get('http://192.168.0.1/status.php');
+    
+                    statusText = driver.findElement(by.id('st_networkstatus'));
+                    console.log('Waiting for connection');
+                    driver.wait(until.elementTextIs(statusText, 'Connected'), WAIT_MLS * 2).then(function() {
+                        console.log('Mode : `' + inetType + '` Connected');                    
+                        driver.quit();                    
+                        ipLocation();
+                    });
                 });
             });
         });
+    });    
+}
+
+function ipLocation() {
+    publicIp.v4().then(myIp => {
+        console.log("My IP address : " + myIp);
+        iplocate(myIp)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.error(err)
+            });            
     });
-});
+}
+
+setupInet(DHCP);
