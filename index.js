bca131a5aff73d50922f434cbb9154d6e8df1ba7
Karthik Chandraraj <ckarthik17@gmail.com>
2018-07-14 02:28:35 +0800

    [CK] Format updated

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 app.get('/vpn', async (req, res) => {
     vpn.connectVPN();
     await sleep(15000);
-    res.send('<h2>VPN setting updated</h2> </br> Visit to check status : <a href="https://www.whatismybrowser.com/detect/ip-address-location">https://www.whatismybrowser.com/detect/ip-address-location</a>');
+    res.send('<h1>VPN setting updated</h1> </br> <h1>Visit to check status : <a href="https://www.whatismybrowser.com/detect/ip-address-location">https://www.whatismybrowser.com/detect/ip-address-location</a></h1>');
 });
 
 app.get('/inet', async (req, res) => {
     vpn.connectDHCP();
     await sleep(15000);
-    res.send('<h2>Normal internet setting updated</h2> </br> Visit to check status : <a href="https://www.whatismybrowser.com/detect/ip-address-location">https://www.whatismybrowser.com/detect/ip-address-location</a>');
+    res.send('<h1>Normal internet setting updated</h1> </br> <h1>Visit to check status : <a href="https://www.whatismybrowser.com/detect/ip-address-location">https://www.whatismybrowser.com/detect/ip-address-location</a></h1>');
 });
 
 function sleep(millis) {
