8a713ccea9f9750b6bf28ca30e365e4b0f9c577f
Karthik Chandraraj <ckarthik17@gmail.com>
2018-07-13 22:09:57 +0800

    [CK] New test using wd.js

--- test.js
+++ test.js
@@ -1,11 +1,39 @@
-let webdriver = require('selenium-webdriver');
-driver = new webdriver.Builder().forBrowser('firefox')
-            .build();
+require('colors');
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
 
-driver.get('https://www.google.com');
- 
-driver.getTitle().then(function (title) {
-    console.log("Title is: " + title);
+var wd = require('wd');
+
+// enables chai assertion chaining
+chaiAsPromised.transferPromiseness = wd.transferPromiseness;
+
+var browser = wd.promiseChainRemote();
+
+// optional extra logging
+browser.on('status', function(info) {
+  console.log(info.cyan);
 });
- 
-driver.quit();
+browser.on('command', function(eventType, command, response) {
+  console.log(' > ' + eventType.cyan, command, (response || '').grey);
+});
+browser.on('http', function(meth, path, data) {
+  console.log(' > ' + meth.magenta, path, (data || '').grey);
+});
+
+/* jshint evil: true */
+browser
+  .init({browserName:'firefox'})
+  .get("http://admc.io/wd/test-pages/guinea-pig.html")
+  .title()
+    .should.become('WD Tests')
+  .elementById('i am a link')
+  .click()
+  .eval("window.location.href")
+    .should.eventually.include('guinea-pig2')
+  .back()
+  .elementByCss('#comments').type('Bonjour!')
+  .getValue().should.become('Bonjour!')
+  .fin(function() { return browser.quit(); })
+  .done();
