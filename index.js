b2c1d0f0b130fd32bc00609e5f5c42c4b7d340ce
Karthik Chandraraj <ckarthik17@gmail.com>
2018-07-14 02:21:29 +0800

    [CK] IP location URL updated

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 app.get('/vpn', async (req, res) => {
     vpn.connectVPN();
     await sleep(15000);
-    res.send('<h2>VPN setting updated</h2> </br> Visit to check status : <a href="https://iplocation.com">https://iplocation.com</a>');
+    res.send('<h2>VPN setting updated</h2> </br> Visit to check status : <a href="https://www.whatismybrowser.com/detect/ip-address-location">https://www.whatismybrowser.com/detect/ip-address-location</a>');
 });
 
 app.get('/inet', async (req, res) => {
     vpn.connectDHCP();
     await sleep(15000);
-    res.send('<h2>Normal internet setting updated</h2> </br> Visit to check status : <a href="https://iplocation.com">https://iplocation.com</a>');
+    res.send('<h2>Normal internet setting updated</h2> </br> Visit to check status : <a href="https://www.whatismybrowser.com/detect/ip-address-location">https://www.whatismybrowser.com/detect/ip-address-location</a>');
 });
 
 function sleep(millis) {
