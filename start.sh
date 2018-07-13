a80735786923978a004638d6e27ad8bac607abf1
Karthik Chandraraj <ckarthik17@gmail.com>
2018-07-13 17:47:05 +0000

    [CK] Start script added

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-xvfb-run java -jar selenium-server-standalone-3.13.0.jar &
-node index.js >> console.out &
+xvfb-run java -jar selenium-server-standalone-3.13.0.jar >> selenium.out &
+forever start index.js >> console.out &
 echo "Started"
